ddecc9bedfaa140d1439e2eb09d8c237cb2b060d
Victor Langa <victorlanga720@gmail.com>
2026-04-18 23:27:49 +0200

    largura do pop up

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -78,8 +78,8 @@ function Tasks(props) {
             </div>
 
             {selectedTask && (
-                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 w-[350px]">
-                    <div className="bg-white p-6 rounded-lg shadow-xl gap-3 w-full max-w-lg relative animate-in fade zoom-in duration-200">
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-xl gap-3 w-full max-w-lg relative animate-in fade zoom-in duration-200 w-[350px]">
                         <button
                             className="absolute top-4 right-4 text-gray-500 cursor-pointer hover:text-gray-700 transition-colors"
                             onClick={() => setSelectedTask(null)}
