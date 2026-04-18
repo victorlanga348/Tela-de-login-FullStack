3eaf99203eae5dba68d417ce80ecb5c6e76ff49f
Victor Langa <victorlanga720@gmail.com>
2026-04-18 21:32:31 +0200

    conectando o servidor correctamente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000 || import.meta.env.VITE_API_URL || "
+    baseURL: "https://tela-de-login-fullstack.onrender.com"
 })
 
 // MÁGICA: Este código abaixo anexa o token automaticamente em TODA requisição
